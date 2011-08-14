f847451d83be0e2925f7b538d82ffea05b6980ad
Alastair Brunton <info@simplyexcited.co.uk>
2011-08-14 22:15:37 +0200

    0.0.4 of the new sagepay gem

--- lib/solon.rb
+++ lib/solon.rb
@@ -7,10 +7,11 @@ require 'solon/customer_data'
 require 'solon/error'
 require 'solon/money'
 require 'solon/post_data'
-require 'solon/sagepay_callback'
 require 'solon/sagepay_response'
 require 'solon/sagepay_services'
+require 'solon/sagepay_callback'
 require 'solon/unique_id'
+require 'solon/net/https_gateway'
 require 'solon/solon_gateway'
 
 
